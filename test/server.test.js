f74a2e4fb8e2860dbed3044b1595fdc6576bbf5d
Luke Schlangen <lukeschlangen@gmail.com>
2016-11-10 11:28:10 -0600

    Every call with one test

--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,20 @@
-var spaceshipPartsStub = [{"id":6374,"name":"Engine","needed":9,"inStock":79},{"id":6377,"name":"Landing Leg","needed":10,"inStock":47},{"id":6984,"name":"First Stage","needed":1,"inStock":9},{"id":8374,"name":"Second Stage","needed":1,"inStock":25},{"id":9394,"name":"Guide Fins","needed":4,"inStock":38}]; // 4
-// var spaceshipPartsStub = [{"id":6374,"name":"Engine","needed":9,"inStock":18},{"id":6377,"name":"Landing Leg","needed":6,"inStock":45},{"id":6984,"name":"First Stage","needed":1,"inStock":5},{"id":8374,"name":"Second Stage","needed":1,"inStock":51},{"id":9394,"name":"Guide Fins","needed":4,"inStock":53}]; // 2
-// var spaceshipPartsStub = [{"id":6374,"name":"Engine","needed":4,"inStock":40},{"id":6377,"name":"Landing Leg","needed":10,"inStock":31},{"id":6984,"name":"First Stage","needed":1,"inStock":3},{"id":8374,"name":"Second Stage","needed":1,"inStock":82},{"id":9394,"name":"Guide Fins","needed":4,"inStock":29}] // 3
-// var spaceshipPartsStub = [{"id":6374,"name":"Engine","needed":7,"inStock":54},{"id":6377,"name":"Landing Leg","needed":3,"inStock":85},{"id":6984,"name":"First Stage","needed":1,"inStock":9},{"id":8374,"name":"Second Stage","needed":1,"inStock":79},{"id":9394,"name":"Guide Fins","needed":4,"inStock":51}] // 7
+
+
+var spaceshipPartsStub1 = [{"id":6374,"name":"Engine","needed":9,"inStock":79},{"id":6377,"name":"Landing Leg","needed":10,"inStock":47},{"id":6984,"name":"First Stage","needed":1,"inStock":9},{"id":8374,"name":"Second Stage","needed":1,"inStock":25},{"id":9394,"name":"Guide Fins","needed":4,"inStock":38}]; // 4
+var spaceshipPartsStub2 = [{"id":6374,"name":"Engine","needed":9,"inStock":18},{"id":6377,"name":"Landing Leg","needed":6,"inStock":45},{"id":6984,"name":"First Stage","needed":1,"inStock":5},{"id":8374,"name":"Second Stage","needed":1,"inStock":51},{"id":9394,"name":"Guide Fins","needed":4,"inStock":53}]; // 2
+var spaceshipPartsStub3 = [{"id":6374,"name":"Engine","needed":4,"inStock":40},{"id":6377,"name":"Landing Leg","needed":10,"inStock":31},{"id":6984,"name":"First Stage","needed":1,"inStock":3},{"id":8374,"name":"Second Stage","needed":1,"inStock":82},{"id":9394,"name":"Guide Fins","needed":4,"inStock":29}] // 3
+var spaceshipPartsStub4 = [{"id":6374,"name":"Engine","needed":7,"inStock":54},{"id":6377,"name":"Landing Leg","needed":3,"inStock":85},{"id":6984,"name":"First Stage","needed":1,"inStock":9},{"id":8374,"name":"Second Stage","needed":1,"inStock":79},{"id":9394,"name":"Guide Fins","needed":4,"inStock":51}] // 7
+var spaceshipPartsStubArray = [];
+spaceshipPartsStubArray.push(spaceshipPartsStub1);
+spaceshipPartsStubArray.push(spaceshipPartsStub2);
+spaceshipPartsStubArray.push(spaceshipPartsStub3);
+spaceshipPartsStubArray.push(spaceshipPartsStub4);
+var spaceshipPartsStub = spaceshipPartsStub1;
+
+
 var proxyquire = require('proxyquire');
 var assert =  require('assert');
-var server = require('../lib/server/app');
-// var server = proxyquire('../lib/server/app', {'./modules/spaceship-parts': spaceshipPartsStub});
+var server = proxyquire('../lib/server/app', {'./modules/spaceship-parts': spaceshipPartsStub});
 
 console.log("Testing the server");
 
@@ -106,20 +115,25 @@ describe('the "/parts" route', function () {
     });
   });
 
-  it('should respond with the values created in the "spaceship-parts.js" module', function (done) {
-    http.get('http://localhost:8000/parts', function (res) {
-      var data = '';
+  // spaceshipPartsStubArray.forEach(function(partsStub){
+    // spaceshipPartsStub = partsStub;
+    it('should respond with the values created in the "spaceship-parts.js" module', function (done) {
+      http.get('http://localhost:8000/parts', function (res) {
+        var data = '';
 
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
+        res.on('data', function (chunk) {
+          data += chunk;
+        });
 
-      res.on('end', function () {
-        assert.equal(data, JSON.stringify(spaceshipPartsStub));
-        done();
+        res.on('end', function () {
+          assert.equal(data, JSON.stringify(spaceshipPartsStub));
+          done();
+        });
       });
     });
-  });
+  // });
+
+
 });
 
 describe('the "/rocketCount" route', function () {
